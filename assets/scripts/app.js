2ad8d60b6ad3f04d8037a679160f14c386395f17
sethwsprague <sethwsprague@gmail.com>
2019-07-29 12:25:59 -0400

    Add UI and APP Scripting based off James HTML

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,6 +6,8 @@
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
+const bookEvents = require('./resource/resource-events')
+
 $(() => {
-  $('#show-books').on('click', resEvents.onViewAvailableVideos)
+  $('#getBooksButton').on('click', bookEvents.onIndexBooks)
 })
--- assets/scripts/books/ui-behavior.js
+++ assets/scripts/books/ui-behavior.js
@@ -1,16 +1,50 @@
 'use strict'
 
 const store = require('../store')
-const showVideosTemplate = require('../templates/video-listing.handlebars')
+const showBooksTemplate = require('../templates/book-listing.handlebars')
+
+
+const successMessage = message => {
+  $('#book-status').text(message).fadeIn(1000)
+  $('#book-status').addClass('success')
+  $('#book-status').removeClass('failure')
+
+  // clear forms
+  $('form').trigger('reset')
+  $('#book-status').text(message).fadeOut(3000)
+}
+
+const failureMessage = message => {
+  $('#book-status').text(message)
+  $('#book-status').addClass('failure')
+  $('#book-status').removeClass('success')
+
+  // clear forms
+  $('form').trigger('reset')
+}
 
 const indexBooksSuccess = responseData => {
+
+  successMessage('Here ara ya books guyzzz!')
   store.books = responseData.books
 
-  const booksHtml = showVideosTemplate({ videos: store.videos })
+  const booksHtml = showBooksTemplate({ books: store.books })
 
-  $('#books-index').append(videosHtml)
+  $('#content').append(booksHtml)
 }
 
 const indexBooksFail = function () {
-  failureMessage('Cannot Load Videos')
+  failureMessage('Cannot Load Books')
+}
+
+const clearBooks = () => {
+  successMessage('Be Gone Books!')
+
+  $('#content').html('')
+}
+
+module.exports = {
+  indexBooksSuccess,
+  indexBooksFail,
+  clearBooks
 }
